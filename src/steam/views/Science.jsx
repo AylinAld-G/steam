003421e17c53046a4028289dd705f6c43dab1821
Aylín Aldana <aylin.aldana2076@alumnos.udg.mx>
2023-12-03 20:04:15 -0600

    Update Science.jsx

--- src/steam/views/Science.jsx
+++ src/steam/views/Science.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import * as React from 'react';
+import { Container, Grid, ThemeProvider, createTheme} from '@mui/material';
+import Footer from '../components/Footer';
+import { PostsList } from '../components/PostsList';
+
+const theme = createTheme();
 
 export const Science = () => {
+
   return (
-    <div>Science</div>
-  )
+    <>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="lg">
+          <Grid>
+            <PostsList/>
+          </Grid>
+          <Footer
+            title="STEAM Intercultural"
+            description="2023"
+          />
+        </Container>
+      </ThemeProvider>
+    </>
+)
 }
