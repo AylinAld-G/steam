f9ac39e4a75560ecdf168ac2d19cd075a74fda96
Aylín Aldana <aylin.aldana2076@alumnos.udg.mx>
2023-12-03 20:03:47 -0600

    Update Maths.jsx

--- src/steam/views/Maths.jsx
+++ src/steam/views/Maths.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { Container, Grid, ThemeProvider, createTheme} from '@mui/material';
+import Footer from '../components/Footer';
+import { PostsList } from '../components/PostsList';
+
+const theme = createTheme();
 
 export const Maths = () => {
   return (
-    <div>Maths</div>
-  )
+    <>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="lg">
+          <Grid>
+            <PostsList/>
+          </Grid>
+          <Footer
+            title="STEAM Intercultural"
+            description="2023"
+          />
+        </Container>
+      </ThemeProvider>
+    </>
+)
 }
