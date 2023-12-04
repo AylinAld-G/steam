7035d25324e5f4906e1b0198455cb027f6f4ae16
Aylín Aldana <aylin.aldana2076@alumnos.udg.mx>
2023-12-03 20:07:07 -0600

    Update authSlice.js

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -5,7 +5,9 @@ export const authSlice = createSlice({
     initialState: {
         status: 'not-authenticated', //authenticated, not-authenticated
         user: {},
-        errorMessage: undefined
+        errorMessage: undefined,
+        users: [],
+        verificationCode: '',
     },
     reducers: {
         onLogin: (state, {payload}) => {
@@ -23,9 +25,25 @@ export const authSlice = createSlice({
           state.user = {};
           state.errorMessage = undefined;
         },
+        onGetUsers: (state, {payload}) => {
+            state.status = 'authenticated';
+            state.users = payload;
+            state.errorMessage = undefined;
+        },
+        onDeleteUser: (state, {payload}) => {
+            state.users = state.users.filter(user => user.user_uuid !== payload.user_uuid);
+            state.errorMessage = undefined;
+        },
+        onUpdateUser: (state, {payload}) => {
+            const updatedUserIndex = state.users.findIndex(user => user.user_uuid === payload.user_uuid);
+            if (updatedUserIndex !== -1) {
+                state.users[updatedUserIndex] = payload.updatedUser;
+            }
+            state.errorMessage = undefined;
+        },
         clearErrorMessage: (state) => {
             state.errorMessage = undefined;
         },
     }
 });
-export const { onChecking, onLogin, onLogout, onRegister, clearErrorMessage } = authSlice.actions;
+export const { onChecking, onLogin, onLogout, onRegister, onGetUsers, onDeleteUser, onUpdateUser, clearErrorMessage } = authSlice.actions;
