5337ea6d4ae363d227363b42aac77924e3016d31
Aylín Aldana <aylin.aldana2076@alumnos.udg.mx>
2024-08-02 11:29:36 -0600

    Update steamApi.js

--- src/api/steamApi.js
+++ src/api/steamApi.js
@@ -7,16 +7,4 @@ const steamApi = axios.create({
     baseURL: VITE_API_URL
 });
 
-//TODO: Configurar interceptores (para interceptar peticiones antes o después de que se hagan)
-steamApi.interceptors.request.use(config => {
-
-    config.headers = {
-        ...config.headers,
-        'x-token': localStorage.getItem('token')   //cualquier petición que haga con el steamApi adicionalmente le coloque este header
-    }
-
-    return config;
-})
-
-
 export default steamApi;
