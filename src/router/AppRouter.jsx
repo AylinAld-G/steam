4396d6e76d70f1284b47cb790f80f68233b2af1c
Aylín Aldana <aylin.aldana2076@alumnos.udg.mx>
2024-11-04 13:40:35 -0600

    Update AppRouter.jsx

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import {SteamRoutes} from '../steam/routes/SteamRoutes';
 import {AuthRoutes} from '../auth/routes';
 import { useAuthStore } from '../hooks';
+import { VerificationCode } from '../auth/pages/VerificationCode';
 
 
 export const AppRouter = () => {
@@ -19,6 +20,7 @@ export const AppRouter = () => {
       }
 
       <Route path='/*' element={<SteamRoutes/>}/>
+      <Route path='/users/:uid/redeem-code' element={<VerificationCode/>}/>
 
     </Routes>
   )
