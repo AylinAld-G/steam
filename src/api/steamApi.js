5976b5b43f1c0ed7d96057e4a43892d86a975dfb
Aylín Aldana <aylin.aldana2076@alumnos.udg.mx>
2024-11-22 15:30:15 -0600

    Update steamApi.js

--- src/api/steamApi.js
+++ src/api/steamApi.js
@@ -8,14 +8,12 @@ const steamApi = axios.create({
     baseURL: VITE_API_URL
 });
 
-steamApi.interceptors.request.use(config => { 
-
-    config.headers = {
-        ...config.headers,
-        'access_token': Cookies.get('access_token')  
+steamApi.interceptors.request.use((config) => {
+    const token = Cookies.get('access_token');
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
     }
-
     return config;
-})
+});
 
 export default steamApi;
