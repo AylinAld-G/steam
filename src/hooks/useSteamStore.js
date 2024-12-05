222fef29e0b843b09a719d7046a7625b981ebb3a
Aylín Aldana <aylin.aldana2076@alumnos.udg.mx>
2024-12-04 22:08:09 -0600

    Update useSteamStore.js

--- src/hooks/useSteamStore.js
+++ src/hooks/useSteamStore.js
@@ -2,32 +2,35 @@ import { useDispatch, useSelector } from 'react-redux';
 import steamApi  from '../api/steamApi';
 import Swal from 'sweetalert2';
 import { onAddNewArticle, onAddNewComment, onDeleteArticle, onDeleteComment, onLoadArticles, onLoadComments, onSetActiveArticle, onUpdateArticle, onUpdateComment } from '../store/steam/steamSlice';
+import { useCallback } from 'react';
+import Cookies from 'js-cookie';
 
 
 export const useSteamStore = () => {
   
     const dispatch = useDispatch();
-    const { publications, activeArticle, comments, activeComment } = useSelector( state => state.steam );
+    const { publications, activeArticle, comments, activeComment, isLoadingArticles } = useSelector( state => state.steam );
     const { user } = useSelector( state => state.auth );
 
-    const setActiveArticle = ( steamArticle ) => {
+    const setActiveArticle = useCallback( (steamArticle ) => {
         dispatch( onSetActiveArticle( steamArticle ) )
-    }
+    },[dispatch])
 
-    const startSavingArticle = async( steamArticle ) => {
-        
+    const startSavingArticle = useCallback(async( steamArticle ) => {
+
         try {
             // Todo bien
-            if( steamArticle.publication_uuid ) {
+            const accessToken = Cookies.get('access_token')
+            if( steamArticle.id_publication ) {
                 // Actualizando
-                await steamApi.put(`/publications/update/${steamArticle.id_publication}`, steamArticle)
+                await steamApi.put(`/publications/update/${steamArticle.id_publication}`, steamArticle, {headers: {Authorization: `${accessToken}`}})
                 dispatch( onUpdateArticle({ ...steamArticle, user }) );
                 return;
 
             } 
 
             // Creando
-            const {data} = await steamApi.post('/publications/add', steamArticle);
+            const {data} = await steamApi.post('/publications/add', steamArticle, {headers: {Authorization: `${accessToken}`}});
             dispatch( onAddNewArticle({ ...steamArticle, id: data.id_publication, user }) );
         
         } catch (error) {
@@ -35,25 +38,27 @@ export const useSteamStore = () => {
             Swal.fire('Error al guardar', error.response.data.msg, 'error');
         }
 
-    }
+    },[dispatch, user])
 
-    const startDeletingArticle = async() => {
+
+    const startDeletingArticle = async () => {
+        console.log(activeArticle)
         try {
-            await steamApi.delete(`/publications/delete/${activeArticle.id_publication}`)
-            dispatch( onDeleteArticle() );
-
+            const accessToken = Cookies.get('access_token');
+            await steamApi.delete(`/publications/delete/${activeArticle[0]}`, {headers: { Authorization: `${accessToken}` }});
+            dispatch(onDeleteArticle());
+            Swal.fire('Éxito', 'El artículo fue eliminado correctamente.', 'success');
         } catch (error) {
-            console.log(error);
-            Swal.fire('Error al eliminar', error.response.data.msg, 'error');
+            console.error(error);
+            Swal.fire('Error al eliminar', error.msg || 'Error desconocido', 'error');
         }
-
-    }
+    };
 
     const startLoadingArticles = async() => {
         try{
             const {data} = await steamApi.get('/publications');
-            dispatch(onLoadArticles(data));
-            console.log(data);
+            
+            dispatch(onLoadArticles(data.publications));
 
         } catch(error){
             console.log('Error cargando artículos');
@@ -67,20 +72,21 @@ export const useSteamStore = () => {
     }
 
     const startSavingComment = async( steamComment ) => {
-        
+        const accessToken = Cookies.get('access_token')
         try {
-            // Todo bien
-            if( steamComment.comment_uuid ) {
+
+            if( steamComment.id_comment) {
+                console.log(steamComment)
                 // Actualizando
-                await steamApi.put(`/comments/update/${steamComment.comment_uuid}`, steamComment)
+                await steamApi.put(`/comments/update/${steamComment.id_comment}`, steamComment, {headers:{Authorization: `${accessToken}`}})
                 dispatch( onUpdateComment({ ...steamComment, user }) );
                 return;
 
             } 
 
             // Creando
-            const {data} = await steamApi.post('/comments/add', steamComment);
-            dispatch( onAddNewComment({ ...steamComment, id: data.comment_uuid, user }) );
+            const {data} = await steamApi.post('/comments/add', steamComment, {headers:{Authorization: `${accessToken}`}});
+            dispatch( onAddNewComment({ ...steamComment, id: data.id_comment, user }) );
         
         } catch (error) {
             console.log(error);
@@ -91,12 +97,15 @@ export const useSteamStore = () => {
 
     const startDeletingComment = async() => {
         try {
-            await steamApi.delete(`/comments/delete/${activeComment.comment_uuid}`)
+            const accessToken = Cookies.get('access_token')
+            console.log(activeComment);
+            await steamApi.delete(`/comments/delete/${activeComment[0]}`, {headers:{Authorization: `${accessToken}`}})
             dispatch( onDeleteComment() );
+            console.log('borrado')
 
         } catch (error) {
             console.log(error);
-            Swal.fire('Error al eliminar', error.response.data.msg, 'error');
+            Swal.fire('Error al eliminar', error.msg, 'error');
         }
 
     }
@@ -105,7 +114,7 @@ export const useSteamStore = () => {
     const startLoadingComments = async(id_publication) => {
         try{
             const {data} = await steamApi.get(`/comments/${id_publication}`);
-            dispatch(onLoadComments(data.id_publication));
+            dispatch(onLoadComments(data.comments));
             console.log("Hello");
 
         } catch(error){
@@ -122,6 +131,7 @@ export const useSteamStore = () => {
         hasArticleSelected: !!activeArticle,
         activeComment,
         comments,
+        isLoadingArticles,
 
         //* Métodos
         startDeletingArticle,
@@ -131,6 +141,7 @@ export const useSteamStore = () => {
         startLoadingComments,
         setActiveComment,
         startSavingComment,
-        startDeletingComment
+        startDeletingComment,
+        
     }
 }
