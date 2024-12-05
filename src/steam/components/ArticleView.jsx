8f86375add3ec894f4e043a2004205c5660b6e32
Aylín Aldana <aylin.aldana2076@alumnos.udg.mx>
2024-12-04 22:16:47 -0600

    Update ArticleView.jsx

--- src/steam/components/ArticleView.jsx
+++ src/steam/components/ArticleView.jsx
@@ -7,7 +7,7 @@ import { useSteamStore } from '../../hooks';
 
 
 function ArticleView(props) {
-  const { post, id, category } = props;
+  const { post, id } = props;
 
   const {t} = useTranslation();
 
@@ -15,75 +15,44 @@ function ArticleView(props) {
     textDecoration: 'none', // Quitar subrayado del Link
   };
 
-  const [isMobile, setIsMobile] = React.useState(false);
-
-  React.useEffect(() => {
-    // Detecta si la pantalla es de 600px o menos
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 600);
-    };
-    handleResize();
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
 
   return (
-    <Grid item xs={16}>
-        <Link to={`/${category}/publications/${id}`} style={cardLinkStyle}>
+    <Grid item xs={12}>
+      <Link to={`/publications/${id}`} style={cardLinkStyle}>
         <CardActionArea component="a">
-        <Card sx={{ display: 'flex', marginTop:"8%", marginBottom:"8%"}}>
-          <CardContent sx={{ flex: 1}}>
-            {isMobile && (
+          <Card sm={{ display: 'flex', marginTop: "8%", marginBottom: "8%", width: '100%' }}>
+            <CardContent md={{ flex: 1 }}>
+                         
               <CardMedia
-              component="img"
-              sx={{ width: '100%', height: '26%', display: 'block' }}
-              image={post.image}
-              alt={post.imageLabel}
-              />)
-            }
-            <Typography component="h2" variant="h5" sx={{fontFamily: 'Didact Gothic, sans-serif' }}>
-              {post.title}
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary" sx={{fontFamily: 'Didact Gothic, sans-serif' }}>
-              {post.author}
-            </Typography>
-            <Typography variant="subtitle1" paragraph sx={{fontFamily: 'Didact Gothic, sans-serif' }}>
-              {post.description}
-            </Typography>
-            <Typography variant="subtitle1" color="primary" sx={{fontFamily: 'Didact Gothic, sans-serif' }}>
-              {t("keepReading")}
-            </Typography>
-          </CardContent>
-          {!isMobile && (
-                <CardMedia
                 component="img"
-                sx={{ width: { xs: '100%', sm: '50%' }, display: { xs: 'none', sm: 'block' } }}
-                image={post.image}
-                alt={post.imageLabel}
-                />
-            )}
- 
-        </Card>
-      </CardActionArea>
+                sx={{
+                  width: '100%',  // Imagen de ancho completo en todas las vistas
+                  height: 'auto',  // Mantener la proporción de la imagen
+                  display: { sm: 'block', md: 'block' },  // Mostrar la imagen en ambas vistas
+                }}
+                image={post[5]}
+              />
+              <Typography component="h2" variant="h5" sx={{ fontFamily: 'Didact Gothic, sans-serif' }}>
+                {post[4]}
+              </Typography>
+              <Typography variant="subtitle1" paragraph sx={{ fontFamily: 'Didact Gothic, sans-serif' }}>
+                {post[6]}
+              </Typography>
+              <Typography variant="subtitle1" color="primary" sx={{ fontFamily: 'Didact Gothic, sans-serif' }}>
+                {t("keepReading")}
+              </Typography>
+            </CardContent>
+            
+          </Card>
+        </CardActionArea>
       </Link>
     </Grid>
-  );
+  )
 }
 
 ArticleView.propTypes = {
-  post: PropTypes.shape({
-    author: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    imageLabel: PropTypes.string,
-    title: PropTypes.string.isRequired,
-  }).isRequired,
-  id: PropTypes.number.isRequired, // id
-  category: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+post: PropTypes.array.isRequired
 };
 
 export default ArticleView;
