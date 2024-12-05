4acc5c3481e29d94129c205c01b911e364e106e5
Aylín Aldana <aylin.aldana2076@alumnos.udg.mx>
2024-12-04 22:10:37 -0600

    Update DeleteDialog.jsx

--- src/steam/views/DeleteDialog.jsx
+++ src/steam/views/DeleteDialog.jsx
@@ -1,27 +1,37 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, ThemeProvider, createTheme } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useSteamStore } from '../../hooks';
+import { onSetActiveArticle } from '../../store/steam/steamSlice';
+import { useDispatch } from 'react-redux';
 
 const theme = createTheme();
 
-export const DeleteDialog = () => {
-    const [open, setOpen] = React.useState(true);
+export const DeleteDialog = ({ onClose }) => {
+    const {startDeletingArticle, activeArticle, startLoadingArticles } = useSteamStore();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const handleClose = () => {
-      setOpen(false);
-      <Link to={'/dashboard'}></Link>
+      onClose();
+      navigate('/dashboard');
     };
-      
-    
-    const handleDelete = (event) => {
-      event.stopPropagation();
-      //startDeletingArticle();
-    }
+
+    const handleDelete = () => {
+      if (activeArticle) {
+          startDeletingArticle().then(()=>{
+            startLoadingArticles()
+          }); 
+          dispatch(onSetActiveArticle(null))
+      } else {
+          console.log("No hay artículo activo");
+      }
+      onClose(); 
+  };
 
   return (
     <ThemeProvider theme={theme}>
     <Dialog
-        open={open}
+        open={true}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
@@ -36,7 +46,7 @@ export const DeleteDialog = () => {
         </DialogContent>
         <DialogActions>
             <Button onClick={handleClose} href='/dashboard'>Cancelar</Button>
-            <Button onClick={handleDelete}>
+            <Button onClick={handleDelete} >
             Aceptar
             </Button>
         </DialogActions>
